f1c6df53f9e703757737b99ef102dc0dcbd29077
lennartdeknikker <me@lenn4rt.com>
2020-03-19 18:28:41 +0100

    test serverless

--- functions/api.js
+++ functions/api.js
@@ -1,8 +1,13 @@
-exports.handler = async (event, context) => {
-  const name = event.queryStringParameters.name || "World";
+import fetch from "node-fetch";
 
-  return {
-    statusCode: 200,
-    body: `Hello, ${name}`
-  };
+const API_ENDPOINT = "https://icanhazdadjoke.com/";
+
+exports.handler = async (event, context) => {
+  return fetch(API_ENDPOINT, { headers: { "Accept": "application/json" } })
+    .then(response => response.json())
+    .then(data => ({
+      statusCode: 200,
+      body: data.joke
+    }))
+    .catch(error => ({ statusCode: 422, body: String(error) }));
 };
